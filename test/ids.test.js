d42327e932b289b4a99d1bc1c9353fbf0b2925f6
aniol-carbo <aniol.carbo@estudiantat.upc.edu>
2021-03-22 19:00:10 +0100

    Add test for the expositions endpoint

--- test/ids.test.js
+++ test/ids.test.js
@@ -19,7 +19,10 @@ describe('Museums', () => {
   beforeEach((done) => {
     Museum.deleteMany({}, (err) => {
       if (err) console.log(err)
-      done()
+      Exposition.deleteMany({}, (err) => {
+        if (err) console.log(err)
+        done()
+      })
     })
   })
 
@@ -41,10 +44,10 @@ describe('Museums', () => {
   })
 
   // eslint-disable-next-line no-undef
-  describe('/GET/:id museum', () => {
+  describe('/GET/:museumId', () => {
     // eslint-disable-next-line no-undef
     it('it should GET a museum by the given id', (done) => {
-      const museum = new Museum({ _id: ObjectId('6048d3d2eaf9c527ba4de26b'), name: 'MACBA', address: 'Plaça Skaters', city: 'Barcelona', country: 'Spain' })
+      const museum = new Museum({ _id: ObjectId('6048d3d2eaf9c527ba4de26a'), name: 'MACBA', address: 'Plaça Skaters', city: 'Barcelona', country: 'Spain', image: 'https://cronicaglobal.elespanol.com/uploads/s1/46/47/88/5/macba.jpeg' })
       museum.save((err, mus) => {
         if (err) console.log(err)
         chai.request(server)
@@ -58,9 +61,39 @@ describe('Museums', () => {
             res.body.museum.should.have.property('address')
             res.body.museum.should.have.property('city')
             res.body.museum.should.have.property('country')
-            res.body.museum.should.have.property('_id').eql(museum.id)
+            res.body.museum.should.have.property('image')
+            res.body.museum.should.have.property('_id').eql(mus.id)
             done()
           })
+      })
+    })
+  })
+
+  // eslint-disable-next-line no-undef
+  describe('/GET/:museumId/:expositionId ', () => {
+    // eslint-disable-next-line no-undef
+    it('it should GET an exposition by the given id', (done) => {
+      const museum = new Museum({ _id: ObjectId('6048d3d2eaf9c527ba4de26b'), name: 'Orsay', address: 'Rue Orsay', city: 'Paris', country: 'France', expositions: ['6048e3baeaf9c527ba4de26d'], image: 'https://cronicaglobal.elespanol.com/uploads/s1/46/47/88/5/macba.jpeg' })
+      const exposition = new Exposition({ _id: ObjectId('6048e3baeaf9c527ba4de26b'), name: 'Main Expo', room: 'Main Hall', descriptions: { ca: 'Catala', es: 'Castellano', en: 'English' }, image: 'https://cronicaglobal.elespanol.com/uploads/s1/46/47/88/5/macba.jpeg' })
+      museum.save((err, mus) => {
+        if (err) console.log(err)
+        exposition.save((err, expo) => {
+          if (err) console.log(err)
+          chai.request(server)
+            .get('/museums/' + mus.id + '/' + expo.id)
+          //   .send(museum)
+            .end((err, res) => {
+              if (err) console.log(err)
+              res.should.have.status(200)
+              res.body.should.be.a('object')
+              res.body.exposition.should.have.property('name')
+              res.body.exposition.should.have.property('room')
+              res.body.exposition.should.have.property('descriptions')
+              res.body.exposition.should.have.property('image')
+              res.body.exposition.should.have.property('_id').eql(expo.id)
+              done()
+            })
+        })
       })
     })
   })
